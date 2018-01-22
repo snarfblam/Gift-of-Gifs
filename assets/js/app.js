229b350e85f0d883e5019e0fb182f2684dafe62d
Tom H <snarfblam@gmail.com>
2018-01-21 19:07:42 -0500

    Incorporated bootstrap. Remembered why I hate bootstrap.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ var GiftApp;
 $(document).ready(function () {
     var giphyQuery = {
         apiKey: "1xj4UUqC8qs1tZW1E83gS7gh9GbgZ6zO",
-        queryUrl: "http://api.giphy.com/v1/gifs/search",
+        queryUrl: "https://api.giphy.com/v1/gifs/search",
 
         /** Sends an AJAX request and calls the callback function with a response */
         sendRequest: function (searchString, callback) {
@@ -24,18 +24,18 @@ $(document).ready(function () {
         },
 
         /** Begins an AJAX request and populates the specified container with the results when the request completes */
-        performSearch: function (searchString, jqOutputContainer) {
+        performSearch: function (searchString, jqOutputContainer, imgConstructor) {
             this.sendRequest(searchString, function (response) {
                 var data = response.data; // {images: {fixed_height_still, fixed_height}, rating}[]
 
-                data.forEach(function (image) {
-                    var element = $("<img>")
-                        .attr("src", image.images.fixed_height_still.url)
-                        .attr("data-src-still", image.images.fixed_height_still.url)
-                        .attr("data-src-anim", image.images.fixed_height.url)
-                        .attr("data-state", "still");
+                jqOutputContainer.empty(); 
 
-                    jqOutputContainer.append(element);
+                data.forEach(function (image) {
+                    var element = imgConstructor(
+                        image.images.fixed_height_still.url,  
+                        image.images.fixed_height.url,
+                        image.rating);
+                    jqOutputContainer.prepend(element);
                 }, this);
             });
         },
@@ -52,16 +52,43 @@ $(document).ready(function () {
             this.createButtons();
             this.uiButtonContainer.on("click", ".search-button", function () {
                 var search = $(this).text();
-                giphyQuery.performSearch(search, self.uiImageContainer);
+                giphyQuery.performSearch(search, self.uiImageContainer, self.imgConstructor);
+            });
+
+            this.uiImageContainer.on("click", ".result-image", function() {
+                var img = $(this);
+                var state = img.attr("data-state");
+                var still = state == "still";
+
+                img.attr("data-state", still ? "anim" : "still");
+                img.attr("src", still ? img.attr("data-src-anim"): img.attr("data-src-still"));
             });
         },
 
         createButtons: function () {
             this.uiButtonContainer.empty();
             this.buttonTexts.forEach(function (txt) {
-                var button = $("<button>").text(txt).addClass("search-button");
+                var button = $("<button>").text(txt).addClass("search-button btn mx-1");
                 this.uiButtonContainer.append(button);
             }, this);
+        },
+
+        imgConstructor: function(stillUrl, animUrl, rating){
+            var img = $("<img>")
+                .attr("src", stillUrl)
+                .attr("data-src-still", stillUrl)
+                .attr("data-src-anim", animUrl)
+                .attr("data-state", "still")
+                .addClass("result-image card-image-top img-fluid");
+
+            var rating = $("<div>").addClass("card-header").text("Rating: " + rating);
+            
+            var card = $("<div>").addClass("card my-1 align-top");
+            card.append(rating).append(img);
+
+            var cardContainer = $("<div>").addClass("col-6 col-md-4 col-lg-3 card-container").append(card);
+            return cardContainer;
+
         },
     };
     GiftApp = giftApp;
